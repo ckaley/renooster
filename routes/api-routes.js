1cf8211eb142eb3be9634e2bd68eaa1be07bb354
Rick Levy <ricklevy21@gmail.com>
2020-07-08 09:47:19 -0600

    removed include from GET routes. GET routes now functioning on backend

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -2,25 +2,17 @@ var db = require("../models");
 
 module.exports = function(app) {
   app.get("/api", function(req, res) {
-    // Here we add an "include" property to our options in our findAll query
-    // We set the value to an array of the models we want to include in a left outer join
-    // In this case, just db.Post
     db.Subscription.findAll({
-      include: [db.Subscription]
     }).then(function(dbSubscription) {
       res.json(dbSubscription);
     });
   });
 
   app.get("/api/:id", function(req, res) {
-    // Here we add an "include" property to our options in our findOne query
-    // We set the value to an array of the models we want to include in a left outer join
-    // In this case, just db.Post
     db.Subscription.findOne({
       where: {
         id: req.params.id
       },
-      include: [db.Subscription]
     }).then(function(dbSubscription) {
       res.json(dbSubscription);
     });
