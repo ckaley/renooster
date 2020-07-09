61cb4633c0fce897bb8379bc8d72fc9e5325256b
Rick Levy <ricklevy21@gmail.com>
2020-07-09 10:52:58 -0600

    PUT api route now operational on backend

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,6 +1,8 @@
 var db = require("../models");
 
 module.exports = function(app) {
+  // @route:  GET /
+  // @desc:   Read all records from subscriptions table
   app.get("/api/subscriptions", function(req, res) {
     db.Subscription.findAll({
     }).then(function(dbSubscription) {
@@ -8,6 +10,8 @@ module.exports = function(app) {
     });
   });
 
+  // @route:  GET /
+  // @desc:   Read one record from subscriptions table by ID
   app.get("/api/:id", function(req, res) {
     db.Subscription.findOne({
       where: {
@@ -18,12 +22,17 @@ module.exports = function(app) {
     });
   });
 
+  // @route:  POST /
+  // @desc:   Add one record to the subscriptions table
   app.post("/api/subscriptions", function(req, res) {
     db.Subscription.create(req.body).then(function(dbSubscription) {
       res.json(dbSubscription);
     });
   });
 
+
+  // @route:  DELETE /
+  // @desc:   Erase one record from the subscriptions table by ID
   app.delete("/api/subscriptions/:id", function(req, res) {
     db.Subscription.destroy({
       where: {
@@ -34,4 +43,21 @@ module.exports = function(app) {
     });
   });
 
+  // @route:  PUT /
+  // @desc:   Update one record from the subscriptions table by ID
+  app.put("/api/subscriptions/:id", function(req, res){
+    //Update takes in object descriping the properties we want to update. Uses WHERE to specify the object that is to be updated
+    db.Subscription.update({
+      name: req.body.name,
+      startDate: req.body.startDate,
+      endDate: req.body.endDate
+      }, {
+        where: {
+          id: req.body.id
+        }
+      })
+      .then(function(dbSubscription) {
+        res.json(dbSubscription)
+      });
+  });
 };
