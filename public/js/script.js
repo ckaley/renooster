10eebb13242034f6c64a6e9f513abed6dc6bd82d
Ann Karpinski <annkarpinski@gmail.com>
2020-07-12 15:32:36 -0600

    got mooing delete modal working

--- public/js/script.js
+++ public/js/script.js
@@ -1,62 +1,66 @@
 $(function () {
-    //==========================================================================================================================
-    //GLOBAL EMPTY VARIABLES
-    //==========================================================================================================================
-    
-    let editId
-    let deleteId
-    let name = ''
-    let startDate = ''
-    let endDate = ''
-    let price = 0.0
-    let frequency = ''
+  //==========================================================================================================================
+  //GLOBAL EMPTY VARIABLES
+  //==========================================================================================================================
 
-    //==========================================================================================================================
-    //FUNCTIONS FOR CRUD FUNCTIONALITY
-    //==========================================================================================================================
-    
-    //CREATE NEW SUBSCRIPTION - front end api call that sends user generated data to server
-    const createSubscription = payload => {
-        $.ajax({
-            method: "POST",
-            url: "/api/subscriptions",
-            data: payload
-        }).then(() => {
-            // reset form inputs
-            $("#name").val("")
-            $("#startDate").val("")
-            $("#endDate").val("")
-            $("#price").val("")
-            $("#frequency").val("")
+  let editId;
+  let deleteId;
+  let name = "";
+  let startDate = "";
+  let endDate = "";
+  let price = 0.0;
+  let frequency = "";
+  var audioElement = new Audio("../sounds/cowmoo.mp3");
 
-            // navigate to "/"
-            window.location.href = "/"
-        }).catch(err => console.log(err))
-    }
+  //==========================================================================================================================
+  //FUNCTIONS FOR CRUD FUNCTIONALITY
+  //==========================================================================================================================
 
-    //SHOW ALL SUBSCRIPTIONS - front end api call that fetches all data from the database and appends to page
-    //--------------------------------------------------------------------------------------------------------------------------
-    const fetchSubscriptions = () => {
-        $.ajax({
-            method: "GET",
-            url: "/api/subscriptions"
-        }).then(subscriptions => {
-            console.log(subscriptions)
+  //CREATE NEW SUBSCRIPTION - front end api call that sends user generated data to server
+  const createSubscription = (payload) => {
+    $.ajax({
+      method: "POST",
+      url: "/api/subscriptions",
+      data: payload,
+    })
+      .then(() => {
+        // reset form inputs
+        $("#name").val("");
+        $("#startDate").val("");
+        $("#endDate").val("");
+        $("#price").val("");
+        $("#frequency").val("");
 
-            // append new node for each subscription
-            subscriptions.forEach(subscription => {
-                // destructure subscription
-                const {
-                    id,
-                    name,
-                    startDate,
-                    endDate,
-                    price,
-                    frequency
-                } = subscription
+        // navigate to "/"
+        window.location.href = "/";
+      })
+      .catch((err) => console.log(err));
+  };
 
-                // format subscription as bootstrap card
-                const card = `
+  //SHOW ALL SUBSCRIPTIONS - front end api call that fetches all data from the database and appends to page
+  //--------------------------------------------------------------------------------------------------------------------------
+  const fetchSubscriptions = () => {
+    $.ajax({
+      method: "GET",
+      url: "/api/subscriptions",
+    })
+      .then((subscriptions) => {
+        console.log(subscriptions);
+
+        // append new node for each subscription
+        subscriptions.forEach((subscription) => {
+          // destructure subscription
+          const {
+            id,
+            name,
+            startDate,
+            endDate,
+            price,
+            frequency,
+          } = subscription;
+
+          // format subscription as bootstrap card
+          const card = `
                     <div class="col-sm-12 col-md-6 col-lg-4 col-xl-3">
                         <div class="card">
                             <div class="card-body">
@@ -66,198 +70,217 @@ $(function () {
                                 <p class="card-text">Price: <i>${price}</i></p>
                                 <p class="card-text">Frequency: <i>${frequency}</i></p>
                                 <button class="btn btn-secondary editBtn" id="${id}">Edit</button>
-                                <button class="btn btn-danger deleteBtn" id="${id}">Delete</button>
-                            </div>
-                        </div>
-                    </div>
-                `
+                                <!-- Button trigger modal -->
+                                <button type="button" class="btn btn-danger deleteBtn" data-toggle="modal" data-target="#exampleModal" id="${id}">
+                                  Delete
+                                </button>
+                                </div>
+                                </div>
+                                </div>
+                                `;
 
-                // append card to dom
-                $("#subscriptions").append(card)
-            })
-        }).catch(err => console.log(err))
-    }
-    
-    //UPDATE SUBSCRIPTION - front end api call that allows user to update data in database by ID
-    //--------------------------------------------------------------------------------------------------------------------------
-    const updateSubscription = payload => {
-        console.log(payload)
-        $.ajax({
-            method: "PUT",
-            url: "/api/edit/" + payload.id,
-            data: payload
-        }).then(() => {
-            // reset form inputs
-            console.log("HERE I AM" + payload)
-            console.log(payload)
-            // fill fields with existing dataTypes
-            $("#name").val(payload.name)
-            $("#startDate").val(payload.startDate)
-            $("#endDate").val(payload.endDate)
-            $("#price").val(payload.price)
-            $("#frequency").val(payload.frequency)
+          //<button class="btn btn-danger deleteBtn" id="${id}">Delete</button>
+          // append card to dom
+          $("#subscriptions").append(card);
+        });
+      })
+      .catch((err) => console.log(err));
+  };
 
-            // navigate to "/"
-            window.location.href = "/"
-        }).catch(err => console.log(err))
-    }
-
-    //DELETE SUBSCRIPTION - front end api call that allows user to delete data from database by ID
-    //--------------------------------------------------------------------------------------------------------------------------
-    const deleteSubscription = deleteId => {
-        console.log(deleteId)
-        $.ajax({
-            method: "DELETE",
-            url: "/api/subscriptions/" + deleteId,
-            data: deleteId
-        }).then(() =>{
-            console.log("You deleted subscription with id: " +deleteId)
-            location.reload()
-        })
-    }
-    
-
-    //==========================================================================================================================
-    //EVENT HANDLERS
-    //==========================================================================================================================
-    
-    //ADDING A NEW SUBSCRIPTION
-    //--------------------------------------------------------------------------------------------------------------------------
-    // handle change event for adding subscription name
-    $("#name").on("change", event => {
-        // destructure event
-        name = event.target.value
+  //UPDATE SUBSCRIPTION - front end api call that allows user to update data in database by ID
+  //--------------------------------------------------------------------------------------------------------------------------
+  const updateSubscription = (payload) => {
+    console.log(payload);
+    $.ajax({
+      method: "PUT",
+      url: "/api/edit/" + payload.id,
+      data: payload,
     })
+      .then(() => {
+        // reset form inputs
+        console.log("HERE I AM" + payload);
+        console.log(payload);
+        // fill fields with existing dataTypes
+        $("#name").val(payload.name);
+        $("#startDate").val(payload.startDate);
+        $("#endDate").val(payload.endDate);
+        $("#price").val(payload.price);
+        $("#frequency").val(payload.frequency);
 
-    // handle change event for adding start date
-    $("#startDate").on("change", event => {
-        // destructure event
-        startDate = event.target.value
-    })
+        // navigate to "/"
+        window.location.href = "/";
+      })
+      .catch((err) => console.log(err));
+  };
 
-    // handle change event for adding end date
-    $("#endDate").on("change", event => {
-        // destructure event
-        endDate = event.target.value
-    })
+  //DELETE SUBSCRIPTION - front end api call that allows user to delete data from database by ID
+  //--------------------------------------------------------------------------------------------------------------------------
+  const deleteSubscription = (deleteId) => {
+    console.log(deleteId);
+    $.ajax({
+      method: "DELETE",
+      url: "/api/subscriptions/" + deleteId,
+      data: deleteId,
+    }).then(() => {
+      console.log("You deleted subscription with id: " + deleteId);
+      location.reload();
+    });
+  };
 
-    // handle change event for adding price
-    $("#price").on("change", event => {
-        // destructure event
-        price = event.target.value
-    })
+  //==========================================================================================================================
+  //EVENT HANDLERS
+  //==========================================================================================================================
 
-    // handle change event for adding frequency
-    $("#frequency").on("change", event => {
-        // destructure event
-        frequency = event.target.value
-    })
+  //ADDING A NEW SUBSCRIPTION
+  //--------------------------------------------------------------------------------------------------------------------------
+  // handle change event for adding subscription name
+  $("#name").on("change", (event) => {
+    // destructure event
+    name = event.target.value;
+  });
 
-    // handle submit event
-    $("#addSubForm").on("submit", event => {
-        // prevent default
-        event.preventDefault()
-        console.log("new subscription added")
-        // create payload
-        const payload = {
-            name: name,
-            startDate: startDate,
-            endDate: endDate,
-            price: price,
-            frequency
-        }
-        // create subscription
-        createSubscription(payload)
-    })
+  // handle change event for adding start date
+  $("#startDate").on("change", (event) => {
+    // destructure event
+    startDate = event.target.value;
+  });
 
-    //EDITING A SUBPSCRIPTION
-    //--------------------------------------------------------------------------------------------------------------------------
-    
-    //event handler for editBtn
-    $("div").on("click", ".editBtn", event => {
-        console.log("gonna edit a sub")
-        console.log(event.target.id)
-        var id = event.target.id
-        editId = id
-        console.log(editId)
-        event.stopPropagation()
+  // handle change event for adding end date
+  $("#endDate").on("change", (event) => {
+    // destructure event
+    endDate = event.target.value;
+  });
 
-        $.ajax({
-            method: "GET",
-            url: "/edit/" + id,
-            data: id
-        }).then(subscriptions => {
-            console.log(subscriptions)
-            window.location.href = `/edit/${id}`
-        })
-    })
+  // handle change event for adding price
+  $("#price").on("change", (event) => {
+    // destructure event
+    price = event.target.value;
+  });
 
-    // handle change event for updating subscription name
-    $("#edit-name").on("change", event => {
-        // destructure event
-        name = event.target.value
-    })
+  // handle change event for adding frequency
+  $("#frequency").on("change", (event) => {
+    // destructure event
+    frequency = event.target.value;
+  });
 
-    // handle change event for updating start date
-    $("#edit-startDate").on("change", event => {
-        // destructure event
-        startDate = event.target.value
-    })
+  // handle submit event
+  $("#addSubForm").on("submit", (event) => {
+    // prevent default
+    event.preventDefault();
+    console.log("new subscription added");
+    // create payload
+    const payload = {
+      name: name,
+      startDate: startDate,
+      endDate: endDate,
+      price: price,
+      frequency,
+    };
+    // create subscription
+    createSubscription(payload);
+  });
 
-    // handle change event for updating end date
-    $("#edit-endDate").on("change", event => {
-        // destructure event
-        endDate = event.target.value
-    })
+  //EDITING A SUBSCRIPTION
+  //--------------------------------------------------------------------------------------------------------------------------
 
-    // handle change event for updating price
-    $("#edit-price").on("change", event => {
-        // destructure event
-        price = event.target.value
-    })
-    // handle change event for updating frequency
-    $("#edit-frequency").on("change", event => {
-        // destructure event
-        frequency = event.target.value
-    })
-    
-    // handle edit event (submit update form)
-    $("#editSubForm").on("submit", event => {
-        // prevent default
-        event.preventDefault()
-        //grab the id from the button
-        console.log("button clicked")
-        // create payload
-        console.log(editId) //---undefined
-        const payload = {
-            id: $("#editSubmitBtn").attr("data"),
-            name: name,
-            startDate: startDate,
-            endDate: endDate,
-            price: price,
-            frequency: frequency
-        }
-        console.log(payload)
-        updateSubscription(payload)
-    })
+  //event handler for editBtn
+  $("div").on("click", ".editBtn", (event) => {
+    console.log("gonna edit a sub");
+    console.log(event.target.id);
+    var id = event.target.id;
+    editId = id;
+    console.log(editId);
+    event.stopPropagation();
 
-    //DELETING A SUBPSCRIPTION
-    //--------------------------------------------------------------------------------------------------------------------------
-    //event handler for deleteBtn
-    $("div").on("click", ".deleteBtn", event => {
-        console.log("gonna delete a sub")
-        console.log(event.target.id)
-        var id = event.target.id
-        deleteId = id
-        console.log(deleteId)
-        event.stopPropagation()
-        deleteSubscription(deleteId)
-    
-    })
-    
-    //==========================================================================================================================
-    //FUNCTION TO POPULATE PAGE
-    //==========================================================================================================================
-    // call function to render all existing subscription records to page
-    fetchSubscriptions()
-})
+    $.ajax({
+      method: "GET",
+      url: "/edit/" + id,
+      data: id,
+    }).then((subscriptions) => {
+      console.log(subscriptions);
+      window.location.href = `/edit/${id}`;
+    });
+  });
+
+  // handle change event for updating subscription name
+  $("#edit-name").on("change", (event) => {
+    // destructure event
+    name = event.target.value;
+  });
+
+  // handle change event for updating start date
+  $("#edit-startDate").on("change", (event) => {
+    // destructure event
+    startDate = event.target.value;
+  });
+
+  // handle change event for updating end date
+  $("#edit-endDate").on("change", (event) => {
+    // destructure event
+    endDate = event.target.value;
+  });
+
+  // handle change event for updating price
+  $("#edit-price").on("change", (event) => {
+    // destructure event
+    price = event.target.value;
+  });
+  // handle change event for updating frequency
+  $("#edit-frequency").on("change", (event) => {
+    // destructure event
+    frequency = event.target.value;
+  });
+
+  // handle edit event (submit update form)
+  $("#editSubForm").on("submit", (event) => {
+    // prevent default
+    event.preventDefault();
+    //grab the id from the button
+    console.log("button clicked");
+    // create payload
+    console.log(editId); //---undefined
+    const payload = {
+      id: $("#editSubmitBtn").attr("data"),
+      name: name,
+      startDate: startDate,
+      endDate: endDate,
+      price: price,
+      frequency: frequency,
+    };
+    console.log(payload);
+    updateSubscription(payload);
+  });
+
+  //DELETING A SUBSCRIPTION
+  //--------------------------------------------------------------------------------------------------------------------------
+  //event handler for deleteBtn
+  $(document).on("click", ".deleteBtn", (event) => {
+    console.log("gonna delete a sub");
+    console.log(event.target.id);
+    var id = event.target.id;
+    deleteId = id;
+    console.log(deleteId);
+    audioElement.play();
+  });
+
+  //==========================================================================================================================
+  //FUNCTION TO POPULATE PAGE
+  //==========================================================================================================================
+  // call function to render all existing subscription records to page
+  fetchSubscriptions();
+
+  //==========================================================================================================================
+  //MODAL FUNCTIONALITY
+  //==========================================================================================================================
+  // event listener for the modal button
+  $("#myModal").on("shown.bs.modal", function () {
+    $("#myInput").trigger("focus");
+  });
+
+  // when user clicks "yes" on warning modal delete subscription
+  $(".yesBtn").on("click", function (event) {
+    console.log(event.target);
+    event.preventDefault();
+    deleteSubscription(deleteId);
+  });
+});
--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,68 +1,67 @@
 //IMPORT DEPENDENCIES
 var db = require("../models");
 
-module.exports = function(app) {
+module.exports = function (app) {
   // @route:  GET /
   // @desc:   Read all records from subscriptions table
-  app.get("/api/subscriptions", function(req, res) {
-    db.Subscription.findAll({
-    }).then(function(dbSubscription) {
+  app.get("/api/subscriptions", function (req, res) {
+    db.Subscription.findAll({}).then(function (dbSubscription) {
       res.json(dbSubscription);
     });
   });
 
   // @route:  GET /
   // @desc:   Read one record from subscriptions table by ID
-  app.get("/api/:id", function(req, res) {
+  app.get("/api/:id", function (req, res) {
     db.Subscription.findOne({
       where: {
-        id: req.params.id
+        id: req.params.id,
       },
-    }).then(function(dbSubscription) {
+    }).then(function (dbSubscription) {
       res.json(dbSubscription);
     });
   });
 
   // @route:  POST /
   // @desc:   Add one record to the subscriptions table
-  app.post("/api/subscriptions", function(req, res) {
-    db.Subscription.create(req.body).then(function(dbSubscription) {
+  app.post("/api/subscriptions", function (req, res) {
+    db.Subscription.create(req.body).then(function (dbSubscription) {
       res.json(dbSubscription);
     });
   });
-
 
   // @route:  DELETE /
   // @desc:   Erase one record from the subscriptions table by ID
-  app.delete("/api/subscriptions/:id", function(req, res) {
+  app.delete("/api/subscriptions/:id", function (req, res) {
     db.Subscription.destroy({
       where: {
-        id: req.params.id
-      }
-    }).then(function(dbSubscription) {
+        id: req.params.id,
+      },
+    }).then(function (dbSubscription) {
       res.json(dbSubscription);
     });
   });
-  
 
   // @route:  PUT /
   // @desc:   Update one record from the subscriptions table by ID
-  app.put("/api/edit/:id", function(req, res){
-    console.log("I am here, please acknowledge that I am here")
+  app.put("/api/edit/:id", function (req, res) {
+    console.log("I am here, please acknowledge that I am here");
     //Update takes in object describing the properties we want to update. Uses WHERE to specify the object that is to be updated
-    db.Subscription.update({
-      name: req.body.name,
-      startDate: req.body.startDate,
-      endDate: req.body.endDate,
-      price: req.body.price,
-      frequency: req.body.frequency
-      }, {
+    db.Subscription.update(
+      {
+        name: req.body.name,
+        startDate: req.body.startDate,
+        endDate: req.body.endDate,
+        price: req.body.price,
+        frequency: req.body.frequency,
+      },
+      {
         where: {
-          id: req.params.id
-        }
-      })
-      .then(function(dbSubscription) {
-        res.json(dbSubscription)
-      });
+          id: req.params.id,
+        },
+      }
+    ).then(function (dbSubscription) {
+      res.json(dbSubscription);
+    });
   });
 };
